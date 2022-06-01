3f93594056b46763233386243eaea3a456a47a00
Steve Brush <steve.brush@blackbaud.com>
2022-06-01 11:30:11 -0400

    try fix npm speed, fix chrome driver

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@
 DEBIAN_FRONTEND=noninteractive
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
+sudo apt-get update
+
 # Install essential packages.
-apt-get install -y apt-transport-https && apt-get update && apt-get install -y --no-install-recommends \
+apt-get install -y \
+  apt-transport-https \
   ca-certificates \
   curl \
   jq \
@@ -31,54 +34,33 @@ curl -fsSL https://deb.nodesource.com/setup_lts | bash - \
   && rm -rf /var/lib/apt/lists/*
 NODE_OPTIONS=--max_old_space_size=3000
 
+# Fix npm install speeds
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo chattr +i /etc/resolv.conf
+
 # Install Azure CLI
 curl -LsS https://aka.ms/InstallAzureCLIDeb | bash \
   && rm -rf /var/lib/apt/lists/* \
   && az extension add -n azure-devops
 
-# Install dependencies for Chrome
-apt-get update && apt-get install -y --no-install-recommends \
-  fonts-liberation \
-  libasound2 \
-  libatk-bridge2.0-0 \
-  libatk1.0-0 \
-  libatspi2.0-0 \
-  libcairo2 \
-  libcups2 \
-  libdbus-1-3 \
-  libdrm2 \
-  libgbm1 \
-  libglib2.0-0 \
-  libgtk-3-0 \
-  libnspr4 \
-  libnss3 \
-  libpango-1.0-0 \
-  libx11-6 \
-  libxcb1 \
-  libxcomposite1 \
-  libxdamage1 \
-  libxext6 \
-  libxfixes3 \
-  libxkbcommon0 \
-  libxrandr2 \
-  && rm -rf /var/lib/apt/lists/*
+# Install Chrome.
+sudo apt-get update
+sudo apt install python-pip
+pip install simplejson
+pip install bs4
+pip install selenium
+sudo apt-get install -y libasound2 libnspr4 libnss3 libxss1 xdg-utils unzip libappindicator1 fonts-liberation
+apt-get -f install
+wget http://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
 
-# Install Chrome
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
-  dpkg -i google-chrome-stable_current_amd64.deb && \
-  apt -f install -y && \
-  rm -f google-chrome-stable_current_amd64.deb
-
-# Install chromedriver
-CHROME_VERSION="google-chrome-stable" && \
-  CHROME_BIN=$(which google-chrome) && \
-  CHROME_MAJOR_VERSION=$(google-chrome -version | sed -E "s/.* ([0-9]+)(\.[0-9]+){3}.*/\1/") && \
-  CHROME_DRIVER_VERSION=$(wget --no-verbose -O - "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_MAJOR_VERSION}") && \
-  echo "Using chromedriver version: $CHROME_DRIVER_VERSION" && \
-  wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip && \
-  rm -rf /opt/selenium/chromedriver && \
-  unzip /tmp/chromedriver_linux64.zip -d /opt/selenium && \
-  rm /tmp/chromedriver_linux64.zip && \
-  mv /opt/selenium/chromedriver /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION && \
-  chmod 755 /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION && \
-  ln -fs /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION /usr/bin/chromedriver
+# Install chromedriver.
+CHROME_VERSION=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
