aca9334ccac259d52e83f4eaa69a68bda5b3f657
Steve Brush <steve.brush@blackbaud.com>
2022-06-02 09:42:17 -0400

    try something

--- setup.sh
+++ setup.sh
@@ -43,30 +43,45 @@ curl -LsS https://aka.ms/InstallAzureCLIDeb | bash \
   && az extension add -n azure-devops
 
 # Install dependencies for Chrome
-apt install -y fonts-liberation \
+apt install -y \
+  gconf-service \
   libasound2 \
-  libatk-bridge2.0-0 \
   libatk1.0-0 \
-  libatspi2.0-0 \
+  libc6 \
   libcairo2 \
   libcups2 \
   libdbus-1-3 \
-  libdrm2 \
-  libgbm1 \
+  libexpat1 \
+  libfontconfig1 \
+  libgcc1 \
+  libgconf-2-4 \
+  libgdk-pixbuf2.0-0 \
   libglib2.0-0 \
   libgtk-3-0 \
   libnspr4 \
-  libnss3 \
   libpango-1.0-0 \
+  libpangocairo-1.0-0 \
+  libstdc++6 \
   libx11-6 \
-  libxcb-dri3-0 \
+  libx11-xcb1 \
   libxcb1 \
   libxcomposite1 \
+  libxcursor1 \
   libxdamage1 \
   libxext6 \
   libxfixes3 \
-  libxkbcommon0 \
-  libxrandr2
+  libxi6 \
+  libxrandr2 \
+  libxrender1 \
+  libxss1 \
+  libxtst6 \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator1 \
+  libnss3 \
+  lsb-release \
+  xdg-utils \
+  wget
 
 # Install Chrome
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
