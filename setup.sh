84e3795984aa87267e22c7fd92be2e09354efabd
Steve Brush <steve.brush@blackbaud.com>
2022-06-03 12:47:25 -0400

    remove autoremove

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ set -exo pipefail
 DEBIAN_FRONTEND=noninteractive
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
-sudo apt-get autoremove
 sudo apt-get clean
 sudo apt-get update
 
